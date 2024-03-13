678a1b80087b9ba9cf181427ac6ebf55f98b6f71
miroslavpenkov <miroslavpenkov12@gmail.com>
2024-03-13 12:10:33 +0100

    update serveur co

--- src/main/java/org/example/testConnexionPC/Serveur.java
+++ src/main/java/org/example/testConnexionPC/Serveur.java
@@ -1,5 +1,7 @@
 package org.example.testConnexionPC;
 
+import java.net.InetAddress;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -19,7 +21,8 @@ public class Serveur extends UnicastRemoteObject implements CommInterface{
         try {
             Serveur serveur = new Serveur();
             Registry registry = LocateRegistry.createRegistry(1099); // Port par défaut
-            registry.rebind("communication", serveur);
+            String addr = "rmi://" + InetAddress.getLocalHost().getHostName() + "/Generation"; Naming.rebind(addr, serveur);
+            //registry.rebind("communication", serveur);
             System.out.println("Serveur RMI prêt.");
         } catch (Exception e) {
             System.err.println("Erreur du serveur RMI : " + e.toString());
